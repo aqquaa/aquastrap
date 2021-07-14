0e554643a002b5449f20e54640e56f576bd9fb37
Sourav R <developer.srv1@gmail.com>
2021-07-14 18:53:27 +0530

    hook as getter property for processing, result & other states - WIP

--- resources/js/public.js
+++ resources/js/public.js
@@ -1,5 +1,38 @@
 function _aquaGenerate(id) {
-    return _replicatePublicMethods(_findComponentById(id).routes, id);
+    const methodsAccessor = _replicatePublicMethods(_findComponentById(id).routes, id);
+
+    let hook = {};
+
+    for (const [name, networkHandler] of Object.entries(methodsAccessor)) {
+        hook = {
+            ...hook,
+            get [name]() {
+                return {
+                    processing: false,
+                    result: null,
+                    submit(form, type = 'POST') {
+                        this.processing = true;
+                        this.result = null;
+
+                        networkHandler(form, type)
+                        .then(res => {
+                            this.result = res;
+                        })
+                        .catch(err => {})
+                        .finally(_ => this.processing = false)
+                    },
+                    put(form) {
+                        this.submit(form, 'PUT');
+                    }
+                }
+            }
+        }
+    }
+
+    return {
+        hook,
+        ...methodsAccessor
+    }
 }
 
 function _registerAquaConfig(id = '') {
