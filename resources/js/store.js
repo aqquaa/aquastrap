2ada0bff6138a693e26d0350e6666f7c6e0734bc
Sourav R <developer.srv1@gmail.com>
2021-07-31 16:48:28 +0530

    route changed to aqua store

---
+++ resources/js/store.js
@@ -0,0 +1,9 @@
+window._aquastrap =  window._aquastrap || {
+    component: [
+        {
+            id: '',
+            config: {}
+        }
+    ],
+    config: {success: () => {}, error: () => {}}
+};
