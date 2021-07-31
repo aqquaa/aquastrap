0cc8957653533d89863138d02fe007dd6754d058
Sourav R <developer.srv1@gmail.com>
2021-07-31 16:47:17 +0530

    aqua request entry endpoint support any http method

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/aquastrap/request', [\Devsrv\Aquastrap\AquaRoute::class, 'Process'])->name('aquastrap.request');
+Route::any('/aquastrap/request', [\Devsrv\Aquastrap\AquaRoute::class, 'Process'])->name('aquastrap.request');
