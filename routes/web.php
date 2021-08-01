7033640149303a2a8fd2311f83ddd4a25160e209
Sourav R <developer.srv1@gmail.com>
2021-08-01 22:44:52 +0530

    aquastrap request entry url changed

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::any('/aquastrap/request', [\Devsrv\Aquastrap\AquaRoute::class, 'Process'])->name('aquastrap.request');
+Route::any('/aquastrap', [\Devsrv\Aquastrap\AquaRoute::class, 'Process'])->name('aquastrap.request');
