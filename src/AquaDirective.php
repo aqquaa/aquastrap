1209b28965a241b654a18a84f7494bfecfc183f6
Sourav R <developer.srv1@gmail.com>
2021-08-11 23:09:19 +0530

    drips always array

--- src/AquaDirective.php
+++ src/AquaDirective.php
@@ -7,24 +7,23 @@ use Devsrv\Aquastrap\GenerateRecipe;
 class AquaDirective
 {
     public static function networkHandler($drips) {
-        $id         = "'". $drips()['id'] . "'";
-        $ingredient = "'". $drips()['ingredient'] . "'";
-        $methods    = str_replace('"', '\'', json_encode($drips()['methods'], JSON_FORCE_OBJECT));
+        $id         = "'". $drips['id'] . "'";
+        $key        = "'". $drips['key'] . "'";
+        $ingredient = "'". $drips['ingredient'] . "'";
+        $methods    = str_replace('"', '\'', json_encode($drips['methods'], JSON_FORCE_OBJECT));
 
-        return "_aquaGenerate(". $id .", ". $ingredient .", ". $methods .")";
+        return "_aquaGenerate(". $id .", ". $key . ", ". $ingredient .", ". $methods .")";
     }
 
     public static function setComponentConfig($drips) {
-        return "_registerAquaConfig('". $drips()['id'] ."')";
+        return "_registerAquaConfig('". $drips['id'] ."')";
     }
 
     public static function linkComponent($link) {
         $class = is_array($link) ? $link[0] : $link;
         $dependencies = is_array($link) && isset($link[1]) ? $link[1] : [];
         
-        return function() use ($class, $dependencies) { 
-            return (new GenerateRecipe($class))->makeWithSuppliedDependencies($dependencies);
-        };
+        return (new GenerateRecipe($class))->makeWithSuppliedDependencies($dependencies);
     }
 
     public static function scripts() {
