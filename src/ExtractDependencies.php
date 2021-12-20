582f78a088201d0288c01f68e173224534ddd300
Sourav R <developer.srv1@gmail.com>
2021-12-20 19:40:36 +0530

    deps extractor refactored into single class

---
+++ src/ExtractDependencies.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Aqua\Aquastrap;
+
+class ExtractDependencies {
+    /**
+     * from class instance extract constructor dependencies
+     * 
+     * @param object $instance
+     * @return array dependencies as assoiative array
+     */
+    public function for(object $instance) : array
+    {
+        $constructorArgs = [];
+
+        $reflectionRef = new \ReflectionClass($instance);
+        $classConstructor = $reflectionRef->getConstructor();
+
+        if($classConstructor) {
+            $parameters = $classConstructor->getParameters();
+
+            foreach($parameters as $param)
+            {
+                $paramName = $param->getName();
+
+                if(property_exists($instance, $paramName)) {
+                    $reflectionProperty = $reflectionRef->getProperty($paramName);
+                    
+                    $constructorArgs[$paramName] = $reflectionProperty->isStatic() 
+                                                    ? $reflectionRef->getStaticPropertyValue($paramName) 
+                                                    : $instance->{$paramName};
+                    continue;
+                }
+
+                if($param->isDefaultValueAvailable()) {
+                    $constructorArgs[$paramName] = $param->getDefaultValue();
+                }
+            }
+        }
+
+        return $constructorArgs;
+    }
+}
