d5c168329ef3b85e6f80fa70f4f79c4d4e402687
Sourav R <developer.srv1@gmail.com>
2021-12-25 17:53:56 +0530

    ingredient store test

---
+++ tests/Pest/Unit/IngredientStoreTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Aqua\Aquastrap\IngredientStore;
+use Illuminate\Support\Facades\Session;
+use Aqua\Aquastrap\DepsLookup\SessionStore;
+use Aqua\Aquastrap\Contracts\DependencyLookupStore;
+
+beforeEach(fn() => app()->instance(DependencyLookupStore::class, new SessionStore));
+
+it('stores and fetches data', function() {
+    $key = md5('random');
+    $value = ['green', 'blue'];
+
+    IngredientStore::set($key, $value);
+
+    $stored = IngredientStore::get($key);
+
+    expect($stored)->toMatchArray($value);
+});
+
+it('the key is prefixed with an unique id for the current user only', function() {
+    $key = md5('random');
+    $value = ['green', 'blue'];
+
+    IngredientStore::set($key, $value);
+
+    $prefix = '_aqua';
+    $uniqueId = Session::getId();
+    
+    $stored = session("$prefix$uniqueId.$key");
+
+    expect($stored)->toMatchArray($value);
+});
