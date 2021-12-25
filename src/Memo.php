91709709862583f0546cb85d7308876a4a4d795f
Sourav R <developer.srv1@gmail.com>
2021-12-25 17:51:54 +0530

    memoizer

---
+++ src/Memo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aqua\Aquastrap;
+
+class Memo
+{
+    protected static array $store = [];
+    
+    public static function getMemoized($className, $key, $setter = null)
+    {
+        if (! isset(static::$store[$className][$key]) && $setter instanceof \Closure) {
+            static::$store[$className][$key] = $setter();
+        }
+
+        return static::$store[$className][$key] ?: null;
+    }
+}
