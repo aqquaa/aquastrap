007a17eb67ae65507a7a7f9c2328859f2e176423
Sourav R <developer.srv1@gmail.com>
2021-12-30 17:20:41 +0530

    aquacomponent includes aquasync

--- src/AquaComponent.php
+++ src/AquaComponent.php
@@ -2,10 +2,13 @@
 
 namespace Aqua\Aquastrap;
 
+use Aqua\Aquastrap\Traits\AquaSync;
 use Illuminate\View\Component as NativeViewComponent;
 
 class AquaComponent extends NativeViewComponent
 {
+    use AquaSync;
+    
     public $_aquaDrips;
 
     public function resolveView()
