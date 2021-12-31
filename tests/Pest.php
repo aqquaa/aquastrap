345cf25dd6ae13c839a58ab548aadef108279257
Sourav R <developer.srv1@gmail.com>
2021-12-31 23:40:12 +0530

    blade string render helper

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,3 +10,8 @@ function isLaravel(int $version)
 {
     return (int) substr(app()->version(), 0, 1) === $version;
 }
+
+function blade(string $blade): string
+{
+    return eval('ob_start(); ?>' . app('blade.compiler')->compileString($blade) . ' <?php return trim(ob_get_clean());');
+}
