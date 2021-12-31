da77e60de6fbfc46b54526e5d448db0f6229f137
Sourav R <developer.srv1@gmail.com>
2021-12-31 23:39:37 +0530

    package asset publish with setup and teardown

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Aqua\Aquastrap\Tests;
 
+use Symfony\Component\Process\Process;
 use Aqua\Aquastrap\AquastrapServiceProvider;
 use Orchestra\Testbench\TestCase as TestbenchTestCase;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
@@ -17,5 +18,33 @@ class TestCase extends TestbenchTestCase
             AquastrapServiceProvider::class,
             TestAquaServiceProvider::class,
         ];
+    }
+
+    protected function setUp(): void
+    {
+        // Code before application created.
+
+        parent::setUp();
+
+        // Code after application created.
+
+        // publish assets
+        $process = new Process(['mkdir', public_path('vendor/aquastrap')]);
+        $process->start();
+        $process->wait();
+
+        $process = new Process(['cp', '-a', __DIR__ .'/../dist/.', public_path('vendor/aquastrap')]);
+        $process->start();
+        $process->wait();
+    }
+
+    protected function tearDown(): void
+    {
+        // flush published assets
+        $process = new Process(['rm', '-rf', public_path('vendor')]);
+        $process->start();
+        $process->wait();
+        
+        parent::tearDown();
     }
 }
