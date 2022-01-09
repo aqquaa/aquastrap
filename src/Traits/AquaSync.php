bdcbad2452be693fc65738006ddde857ac7dede9
Sourav R <developer.srv1@gmail.com>
2022-01-09 17:03:22 +0530

    trait has all properties with proper description for ide hint

--- src/Traits/AquaSync.php
+++ src/Traits/AquaSync.php
@@ -9,13 +9,44 @@ trait AquaSync
     use Notification;
 
     /**
+     * The middlewares to apply when any of the allowed method of this class gets called.
+     *
+     * @var array
+     */
+    protected static $middlewares = [];
+
+    /**
+     * Whether the request is allowed,
+     * useful in authorization
+     *
+     * @var bool
+     */
+    public function allowed() : bool {
+        return true;
+    }
+
+    /**
+     * The methods to restrict from being called.
+     *
+     * @var array
+     */
+    protected static $aquaGuarded = [];
+
+    /**
+     * Only the methods to allow being called.
+     *
+     * @var array
+     */
+    protected static $aquaCallable = [];
+
+    /**
      * I need $_aquaDrips to be available in the view
      *
      */
     protected function aquaRecipes(): array
     {
         return [
-            '_aquaDrips' => (new Recipe($this))->generate(),
+            '_aquaDrips' => (new Recipe)($this),
         ];
     }
 }
